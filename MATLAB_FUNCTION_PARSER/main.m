0c510f0a000f5324079929c6b99133bb3334e640
Matteo Danieletto <matteo.danieletto@gmail.com>
2014-09-10 16:28:19 -0700

    UPDATE matlab functions

--- MATLAB_FUNCTION_PARSER/main.m
+++ MATLAB_FUNCTION_PARSER/main.m
@@ -4,49 +4,47 @@
 
 function main(M ,EXP,varDir ,nameEXP , ctrlOther)
 
+    simuTime=600; 
+    sample=0.1;
+    collect = 0; % set to 1 only when we found the correct starting point
+    startTime = 200;
+    if(checkAllFile() == 1)
+        load 'CongestionWindow_node_0.txt'
+        for i = 1:length(CongestionWindow_node_0)
+            num2str(CongestionWindow_node_0(i,1),'%.1f');
+            CongestionWindow_node_0(i,1) = str2num(ans);
+        end
 
+        while (collect==0)
+           startTime=startTime+1;
+           if (find(CongestionWindow_node_0(:,1)*10==(startTime))~=0)
+               collect=1;
+           end
+        end
 
-simuTime=600; 
-sample=0.1;
-collect = 0; % set to 1 only when we found the correct starting point
-startTime = 200;
+        % startTime=20;
+        startTime=startTime/10;
+        fprintf('\nPHASE 1: processing parameters H CHAIN... \n\n')
+        processParametersCHAIN1(startTime , simuTime, sample , M, EXP,varDir ,nameEXP ,ctrlOther ); 
 
-load 'CongestionWindow_node_0.txt'
-for i = 1:length(CongestionWindow_node_0)
-    num2str(CongestionWindow_node_0(i,1),'%.1f');
-    CongestionWindow_node_0(i,1) = str2num(ans);
-end
+        startTime = 200;
+        % startTime = startTime * 10;
+        load 'CongestionWindow_node_5.txt'
+        for i = 1:length(CongestionWindow_node_5)
+            num2str(CongestionWindow_node_5(i,1),'%.1f');
+            CongestionWindow_node_5(i,1) = str2num(ans);
+        end
 
-while (collect==0)
-   startTime=startTime+1;
-   if (find(CongestionWindow_node_0(:,1)*10==(startTime))~=0)
-       collect=1;
-   end
-end
+        while (collect==0)
+           startTime=startTime+1;
+           if (find(CongestionWindow_node_5(:,1)*10==(startTime))~=0)
+               collect=1;
+           end
+        end
 
-% startTime=20;
-startTime=startTime/10;
-fprintf('\nPHASE 1: processing parameters H CHAIN... \n\n')
-processParametersCHAIN1(startTime , simuTime, sample , M, EXP,varDir ,nameEXP ,ctrlOther ); 
-
-startTime = 200;
-% startTime = startTime * 10;
-load 'CongestionWindow_node_5.txt'
-for i = 1:length(CongestionWindow_node_5)
-    num2str(CongestionWindow_node_5(i,1),'%.1f');
-    CongestionWindow_node_5(i,1) = str2num(ans);
-end
-
-while (collect==0)
-   startTime=startTime+1;
-   if (find(CongestionWindow_node_5(:,1)*10==(startTime))~=0)
-       collect=1;
-   end
-end
-
-startTime=startTime/10;
-% startTime=20;
-fprintf('\nPHASE 1: processing parameters V CHAIN... \n\n')
-processParametersCHAIN2(startTime , simuTime, sample, M, EXP ,varDir ,nameEXP , ctrlOther ); 
-
+        startTime=startTime/10;
+        % startTime=20;
+        fprintf('\nPHASE 1: processing parameters V CHAIN... \n\n')
+        processParametersCHAIN2(startTime , simuTime, sample, M, EXP ,varDir ,nameEXP , ctrlOther ); 
+    end
 end
