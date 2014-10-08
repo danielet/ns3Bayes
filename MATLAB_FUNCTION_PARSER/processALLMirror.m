7a7690aaf4f8aadc6b1a74880ecd15646cfb78c8
Danieletto Matteo <danielet@login.dei.unipd.it>
2014-10-08 03:48:35 +0200

    fix mirror

--- MATLAB_FUNCTION_PARSER/processALLMirror.m
+++ MATLAB_FUNCTION_PARSER/processALLMirror.m
@@ -16,30 +16,35 @@ for gi=1:size(folders,1)
         varDir='M5_LEARN_MOVE';
         varDir= strcat(PATH,'/', varDir) ;
         mkdir(varDir);
+	correctFolder=1;
         elseif strcmp(folders(gi,1).name,'NEWRUN_nakagamiM10')
             pathDir=folders(gi,1).name;
             offM=2; % M=10
             varDir='M10_LEARN_MOVE' ;
             varDir= strcat(PATH,'/', varDir) ;
             mkdir(varDir);
+		correctFolder=1;
             elseif strcmp(folders(gi,1).name,'NEWRUN_nakagamiM20')
                 pathDir=folders(gi,1).name;
                 offM=3; % M=20
                 varDir='M20_LEARN_MOVE' ;
                 varDir= strcat(PATH,'/', varDir) ;
-                mkdir(varDir);
+		correctFolder=1;
+		mkdir(varDir);
                 elseif strcmp(folders(gi,1).name,'NEWRUN_nakagamiM50')
                     pathDir=folders(gi,1).name;
                     offM=4; % M=50                       
                     varDir='M50_LEARN_MOVE' ;
                     varDir= strcat(PATH,'/', varDir) ;
                     mkdir(varDir);
-                    elseif strcmp(folders(gi,1).name,'NEWRUN_nakagamiM100')
+			correctFolder=1;
+		elseif strcmp(folders(gi,1).name,'NEWRUN_nakagamiM100')
                         pathDir=folders(gi,1).name;
                         offM=5; % M=100
                         varDir='M100_LEARN_MOVE' ;
                         varDir= strcat(PATH,'/', varDir) ;
                         mkdir(varDir);
+			correctFolder=1;
 %     end
     end
     
@@ -58,7 +63,7 @@ for gi=1:size(folders,1)
                 varDir = fullfile(varDir);
                 mkdir(varDir, nameEXP);
                 tic
-                main( varDir ,nameEXP ,ctrOTHER); % execute the script for processing and testing 
+                mainMirror( varDir ,nameEXP ,ctrOTHER); % execute the script for processing and testing 
                 toc
                 cd ..
                 ii=ii+1;
